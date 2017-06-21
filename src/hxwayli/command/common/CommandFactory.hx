73907925e238975ec0f29fdbbd52967453e41362
bynuff <igor.bulankov@gmail.com>
2017-06-21 15:26:30 +0300

    remove unused init cmd

--- src/hxwayli/command/common/CommandFactory.hx
+++ src/hxwayli/command/common/CommandFactory.hx
@@ -4,8 +4,6 @@ class CommandFactory {
 
     public static function getByName(commandType:CommandType, args:Array<String>):ICommand {
         return switch(commandType) {
-//            case CommandType.Init:
-//                new InitCommand();
 //            case CommandType.LsLi:
             case CommandType.Pack:
                 new PackCommand(args.shift());
