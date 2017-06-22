053a0365518e55ac0a97dca0bf87068cc330505c
bynuff <igor.bulankov@gmail.com>
2017-06-22 19:58:27 +0300

    uninst fixes for win

--- src/hxwayli/command/UninstCommand.hx
+++ src/hxwayli/command/UninstCommand.hx
@@ -16,8 +16,9 @@ class UninstCommand extends EnvCommand {
         // TODO: remove lib hxwayli from haxelib
         // TODO: refactor me
         if (~/windows/i.match(Sys.systemName())) {
-            Sys.command("rmdir", ["/s", "/q", Path.join([Sys.getEnv("HAXEPATH"), "wayli"])]);
-            Sys.command("del", ["/s", "/f", "/q", Path.join([Sys.getEnv("HAXEPATH"), "wayli.exe"])]);
+            Sys.command("cd", [new Path(Sys.getEnv("HAXEPATH")).dir]);
+            Sys.command("rmdir", ["/s", "/q", "wayli"]);
+            Sys.command("del", ["/s", "/f", "/q", "wayli.exe"]);
         } else {
             Sys.command("sudo", ["rm", "-rf", Path.join(["/usr/local/lib/haxe", "wayli"])]);
             Sys.command("sudo", ["rm", "-rf", "/usr/local/bin/wayli"]);
